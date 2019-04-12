2012bebd8d654ee5f64bc73db8d0d4f3ed586254
josemart <jose.martin@nentgroup.com>
2019-04-12 15:19:39 +0200

    POM

---
+++ POMdemo/Tests/LoginTest.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+import unittest
+from POMdemo.Pages.loginPage import LoginPage
+from POMdemo.Pages.homePage import HomePage
+
+class LoginTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(10)
+
+    def test_login(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+        # self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # self.driver.find_element_by_id("btnLogin").click()
+        # self.driver.find_element_by_id("welcome").click()
+        # self.driver.find_element_by_link_text("Logout").click()
+        # time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test completed")
+
+
